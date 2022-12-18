5247633b2400a6beeb694683ec4bc7da30f2a53d
Svutlana <fire_fly_74@mail.ru>
2022-12-18 18:09:55 +0500

    home05

---
+++ home05/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void CounterEven(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            counter = counter + 1;
+        }
+    }
+    Console.Write(counter);
+}
+
+int[] CreateMassive(int count, int begin, int end)
+{
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = new Random().Next(begin, end + 1);
+    }
+    return arr;
+}
+
+// Задать массив заполненный случайными положительными трёхзначными числами
+int[] array = CreateMassive( 5, 100, 1000);
+Console.WriteLine(String.Join(" ", array));
+// найти количество чётных чисел в массиве
+CounterEven(array); 
